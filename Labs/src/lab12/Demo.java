a20de2d60eb8f1efa8e43661167798514a546722
Marwan srv-pi.lan <Marwan@srv-pi.lan>
2021-11-25 11:58:53 -0700

    clear unsed vars

--- Labs/src/lab12/Demo.java
+++ Labs/src/lab12/Demo.java
@@ -1,6 +1,5 @@
 package lab12;
 
-import java.io.IOException;
 import java.sql.*;
 
 public class Demo {
@@ -10,14 +9,11 @@ public class Demo {
 
   public static void main(String[] args) {
     Connection conn = null;
-    Statement  stmt = null;
     PreparedStatement pstm = null;
     try {
       conn = DriverManager.getConnection(DB_URL, DB_USER, DB_PASS);
-      stmt = conn.createStatement();
       pstm = conn.prepareStatement("select * from countries WHERE `name` LIKE ? LIMIT 10;");
       pstm.setString(1, "%Saudi%");
-      //ResultSet rs = stmt.executeQuery("select * from countries LIMIT 10;");
       ResultSet rs = pstm.executeQuery();
       while (rs.next()) {
         System.out.printf("%d\t%s\n", rs.getInt(1), rs.getString(2));
